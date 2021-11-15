16b59be25ff1f4f88fe1c7b5da4bc72e47c16baa
mzprojects <mariusz.zdeb.projects@gmail.com>
2021-11-15 20:20:12 +0100

    Update Footer.js

--- projekty/006_react_xD/project2/src/components/footer/Footer.js
+++ projekty/006_react_xD/project2/src/components/footer/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./footer.css";
 import "../copyright/Copyright";
 import Copyright from "../copyright/Copyright";
+import { render } from "react-dom";
 
 const elements1 = ["Regulamin", "FAQ", "O nas", "Kontakt"];
+const elements2 = ["cono", "cono2", "cono3"];
 
 const items = [];
 
@@ -15,7 +17,14 @@ function Footer(props) {
   return (
     <div>
       <footer>
-        <ul>{items}</ul>
+        <ul>
+          {elements1.map((value, index) => {
+            return <li>{value}</li>;
+          })}
+          {elements2.map((value, index) => {
+            return <li>{value}</li>;
+          })}
+        </ul>
 
         <Copyright year="2021" />
       </footer>
