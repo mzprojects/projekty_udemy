0f5be6f4a5741b066f3cddb484c0e31b874c5d33
mzprojects <mariusz.zdeb.projects@gmail.com>
2021-11-16 20:53:19 +0100

    Destructuring props

--- projekty/006_react_xD/project2/src/App.js
+++ projekty/006_react_xD/project2/src/App.js
@@ -6,9 +6,33 @@ import Articles from "./components/Articles/Articles";
 import Dupa from "./components/Dupa/Dupa";
 import Footer from "./components/footer/Footer";
 import Navigation from "./components/navigation/Navigation";
-
-const elements1 = ["Regulamin", "FAQ", "O nas", "Kontakt"];
-const elements2 = ["cono", "cono2", "cono3"];
+const aprops = {
+  attraction: {
+    address1: "350 5th Ave",
+    address2: "",
+    average_rating: 4,
+    city: "New York",
+    country: "US",
+    display_address: ["350 5th Ave", "New York, NY 10118"],
+    id: 9,
+    latitude_coordinate: "40.748442285082",
+    location: {
+      created_at: "2018–03–07T03:56:20.717Z",
+      id: 1,
+      latitude_coordinate: 40.712775,
+      longitude_coordinate: -74.005973,
+    },
+  },
+  auth: {
+    loggedIn: true,
+    loggingIn: false,
+    token: 2104890123,
+  },
+  links: {
+    elements1: ["Regulamin", "FAQ", "O nas", "Kontakt"],
+    elements2: ["cono", "cono2", "cono3"],
+  },
+};
 
 const companyData = {
   email: "contact@email.com",
@@ -16,18 +40,16 @@ const companyData = {
   street: "Wacławska",
 };
 
-function App() {
-  return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/articles" element={<Articles />}></Route>
-        <Route path="/dupa" element={<Dupa />}></Route>
-      </Routes>
-      <Footer elements1={elements1} elements2={elements2} />
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/articles" element={<Articles />}></Route>
+      <Route path="/dupa" element={<Dupa />}></Route>
+    </Routes>
+    <Footer {...aprops} />
+  </Router>
+);
 
 export default App;
--- projekty/006_react_xD/project2/src/components/footer/Footer.js
+++ projekty/006_react_xD/project2/src/components/footer/Footer.js
@@ -6,13 +6,18 @@ import { Link, Routes } from "react-router-dom";
 
 export default class Footer extends React.Component {
   render() {
+    const {
+      attraction: { address1 },
+      auth,
+      links: { elements1, elements2 },
+    } = this.props;
     return (
       <footer>
         <ul>
-          {this.props.elements1.map((value) => (
+          {elements1.map((value) => (
             <li>{value}</li>
           ))}
-          {this.props.elements2.map((value) => (
+          {elements2.map((value) => (
             <li>{value}</li>
           ))}
         </ul>
@@ -23,3 +28,32 @@ export default class Footer extends React.Component {
     );
   }
 }
+
+// const aprops = {
+//   attraction: {
+//     address1: "350 5th Ave",
+//     address2: "",
+//     average_rating: 4,
+//     city: "New York",
+//     country: "US",
+//     display_address: ["350 5th Ave", "New York, NY 10118"],
+//     id: 9,
+//     latitude_coordinate: "40.748442285082",
+//     location: {
+//       created_at: "2018–03–07T03:56:20.717Z",
+//       id: 1,
+//       latitude_coordinate: 40.712775,
+//       longitude_coordinate: -74.005973,
+//     },
+//   },
+//   auth: {
+//     loggedIn: true,
+//     loggingIn: false,
+//     token: 2104890123
+//   },
+//     links: {
+//     elements1:["Regulamin", "FAQ", "O nas", "Kontakt"],
+//     elements2:  ["cono", "cono2", "cono3"]
+//     }
+
+// };
